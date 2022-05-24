a25672ff47347edd5aecef48468e68e63ab33ec4
Prabhas-M <prabhasmuta99@gmail.com>
2022-05-24 10:01:28 +0530

    updating

---
+++ public/js/home.js
@@ -0,0 +1,4 @@
+document.querySelector(".menubutton").addEventListener("click", function (){
+        document.querySelector(".menubutton").classList.toggle("clicked");
+        document.querySelector(".menubar").classList.toggle("display");
+       });
